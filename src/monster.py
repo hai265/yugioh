88f07f923ca721d0b12cc7a41a4abf755d59460a
Hai <cognation100@gmail.com>
2022-04-07 14:54:59 -0400

    remove extra file

--- src/monster.py
+++
@@ -1,9 +0,0 @@
-from src.card import Card
-
-
-class Monster(Card):
-    def __init__(self, name, description, card_type, attack, defense):
-        super(Monster, self).__init__(name, description, card_type)
-        self.attack = attack
-        self.defense = defense
-        
