e48679ec7a7305448e7046532d46f5244a20ce86
Aravind Ganeshan <aganesha@terpmail.umd.edu>
2022-04-30 20:37:45 -0400

    Summoning and defense position

--- tests/test_card.py
+++ tests/test_card.py
@@ -18,40 +18,20 @@ class TestGameMethods(unittest.TestCase):
         deck = create_deck_from_preset("sources/preset1")
         real_deck = [create_card("Hitotsu-Me Giant"), create_card("Dark Magician"), create_card("The Fierce Knight"),
                      create_card("Mammoth Graveyard"), create_card("Silver Fang"),
-                     create_card("Curtian of the Dark One"), create_card("Tomozaurus"), create_card("Feral Imp")]
+                     create_card("Curtain of the Dark One"), create_card("Tomozaurus"), create_card("Feral Imp")]
         self.assertEqual(len(real_deck), len(deck))
         for i in range(8):
             self.assertEqual(deck[0].name, real_deck[0].name)
 
     def test_create_deck_from_array(self):
         deck = create_deck_from_array(["Hitotsu-Me Giant", "Dark Magician", "The Fierce Knight", "Mammoth Graveyard",
-                                       "Silver Fang", "Curtian of the Dark One", "Tomozaurus", "Feral Imp"])
+                                       "Silver Fang", "Curtain of the Dark One", "Tomozaurus", "Feral Imp"])
         real_deck = [create_card("Hitotsu-Me Giant"), create_card("Dark Magician"), create_card("The Fierce Knight"),
                      create_card("Mammoth Graveyard"), create_card("Silver Fang"),
-                     create_card("Curtian of the Dark One"), create_card("Tomozaurus"), create_card("Feral Imp")]
+                     create_card("Curtain of the Dark One"), create_card("Tomozaurus"), create_card("Feral Imp")]
         self.assertEqual(len(real_deck), len(deck))
         for i in range(8):
             self.assertEqual(deck[0].name, real_deck[0].name)
-
-
-class TestCard(unittest.TestCase):
-
-    def test_display_card_card_correct_inputs(self):
-        test_card_1 = Card("Hitotsu-Me Giant", "A behemoth")
-        test_card_2 = Card("Dark Magician", "None")
-        test_card_3 = Card("Gaia The Fierce Knight", "None")
-
-        result_1 = test_card_1.display_card()
-        self.assertEqual(result_1["name"], "Hitotsu-Me Giant")
-        self.assertEqual(result_1["description"], "A behemoth")
-
-        result_2 = test_card_2.display_card()
-        self.assertEqual(result_2["name"], "Dark Magician")
-        self.assertEqual(result_2["description"], "None")
-
-        result_3 = test_card_3.display_card()
-        self.assertEqual(result_3["name"], "Gaia The Fierce Knight")
-        self.assertEqual(result_3["description"], "None")
 
 
 class TestMonster(unittest.TestCase):
@@ -61,11 +41,11 @@ class TestMonster(unittest.TestCase):
         test_card_3 = Monster("Gaia The Fierce Knight", "None", "Earth", "Warrior", 7, 2300, 2100)
 
         card1_repr = "name:Hitotsu-Me Giant, level:4, attribute:Earth, type:Beast-Warrior, atk:1200, def:1000, " \
-                     "position:atk"
+                     "face position:up, battle position:atk"
         card2_repr = "name:Dark Magician, level:7, attribute:Dark, type:Spellcaster, atk:2500, def:2100, " \
-                     "position:atk"
+                     "face position:up, battle position:atk"
         card3_repr = "name:Gaia The Fierce Knight, level:7, attribute:Earth, type:Warrior, atk:2300, def:2100, " \
-                     "position:atk"
+                     "face position:up, battle position:atk"
 
         result1 = repr(test_card_1)
         result2 = repr(test_card_2)
